4aaca78e1fe60c68089b13107223dc9dd3ddf35a
EricLich <erick_lich@hotmail.com>
2023-01-20 10:44:56 -0300

    add montserrat font and design system

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div className="text-red-700">Home</div>;
+  return <div className="text-red-700 font-mont">Home</div>;
 };
 
 export default Home;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +6,32 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'lgMaxWidth': '1248px',
+        'smMaxW': '90%',
+      },
+      fontFamily: {
+        'mont': ['"Montserrat"', ...defaultTheme.fontFamily.sans]
+      },
+      fontSize: {
+        'lgTitle': '48px',
+        'smTitle': '40px',
+        'lgHeroDesc': '24px',
+        'smHeroDesc': '10px',
+        'normalText': '18px',
+      },
+      lineHeight: {
+        'lgTitleLeading': '59px',
+        'smTitleLeading': '49px',
+        'lgHeroDescLeading': '29px',
+        'smHeroDescLeading': '12px',
+      },
+      colors: {
+        'breedsBg': '#E3E1DC',
+        'normalText': '#291507',
+      }
+    },
   },
   plugins: [],
 }
